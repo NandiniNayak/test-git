586f724735a6e83dacd7ac6f3de99c159526743b
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-03-28 11:00:46 +1100

    pushing to side branch

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,11 @@ end
 
 result = sum(4,4)
 puts "sum of two number is #{result}"
+
+def subtract(num1,num2)
+  diff = num1 - num2
+  return diff
+end
+
+result = subtract(4,4)
+puts "difference of two numbers are #{result}"
